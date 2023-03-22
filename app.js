d61b95e4f85352df6eb7bfc2c0f9f60f4f2a41d9
Mikhail Yarashevich <bigbottleapps@gmail.com>
2023-03-22 10:15:29 +0100

    test

--- app.js
+++ app.js
@@ -5,32 +5,34 @@ const bodyParser = require('body-parser');
 const usersRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
 const tripsRouter = require('./routes/trips');
+const defaultRouter = require('./routes/default');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 80;
 
-const isDebug = false
-
-let mongoUrl = 'mongodb+srv://yrshvchstudio:nnAzaZwpALAOIyEB@pingocluster.jfl4hmk.mongodb.net/?retryWrites=true&w=majority'
-if (isDebug) {
-    mongoUrl = 'mongodb://localhost:27017/pingo'
-}
-
-mongoose.connect(mongoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log('MongoDB Connected'))
-    .catch((error) => {
-        console.error('MongoDB Connection Error:', error);
-    });
+// const isDebug = false
+//
+// let mongoUrl = 'mongodb+srv://yrshvchstudio:nnAzaZwpALAOIyEB@pingocluster.jfl4hmk.mongodb.net/?retryWrites=true&w=majority'
+// if (isDebug) {
+//     mongoUrl = 'mongodb://localhost:27017/pingo'
+// }
+//
+// mongoose.connect(mongoUrl, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
+//     .then(() => console.log('MongoDB Connected'))
+//     .catch((error) => {
+//         console.error('MongoDB Connection Error:', error);
+//     });
 
 app.use(bodyParser.json());
+app.use('/', defaultRouter);
 app.use('/users', usersRouter);
 app.use('/trips', tripsRouter);
 app.use('/auth', authRouter);
 
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+    console.log(`Server has been started at port:${port}`);
 });
---
+++ routes/default.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    res.status(200).send({isWorking: true});
+});
+
+module.exports = router;
