d8af42bcdceaec20103b5e2c45ea6b6e510808a7
Mikhail Yarashevich <bigbottleapps@gmail.com>
2023-04-18 16:09:21 +0200

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const auth = require("../middleware/auth");
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.post('/login', async (req, res) => {
     try {
         const {email, password, fcmToken} = req.body;
         const user = await User.findOneAndUpdate({email}, {fcmToken})
