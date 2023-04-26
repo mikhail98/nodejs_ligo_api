6bcbed36dacb63bbdc019a9ce018e6b3d21429c2
Mikhail Yarashevich <bigbottleapps@gmail.com>
2023-04-26 18:15:55 +0200

    headers update

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ mongoose.connect(mongoUrl, {
 app.use(bodyParser.json())
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With")
+    res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With", "x-access-token")
+
     next()
 });
 app.use('/', defaultRouter)
