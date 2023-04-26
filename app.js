de81fe2bfb6820621f14557071bb34ade228404f
Mikhail Yarashevich <bigbottleapps@gmail.com>
2023-04-26 18:27:38 +0200

    push

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ mongoose.connect(mongoUrl, {
 app.use(bodyParser.json())
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With", "x-access-token")
+    res.header("Access-Control-Allow-Headers", "content-type, x-requested-with", "x-access-token")
 
     next()
 });
