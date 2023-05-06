32e160f1914dab16f44c0b6ab51d2943e8c68944
Mikhail Yarashevich <bigbottleapps@gmail.com>
2023-05-06 17:55:33 +0200

    error

--- errors/errors.js
+++ errors/errors.js
@@ -1,19 +1,23 @@
-const wrongPassword = createError("Wrong password")
-const noSuchUser = createError("No such user")
-const noSuchTrip = createError("No such trip")
-const noSuchParcel = createError("No such parcel")
-const userExits = createError("This user also exists")
-const notADriver = createError("Not a driver")
-const requiredToken = createError("Token is required")
-const invalidToken = createError("Invalid token")
-const driverHasActiveTrip = createError("Driver has active an trip")
-const pointsAreTheSame = createError("startPoint can't be equal to endPoint")
-const youNeedAdminRights = createError("You need to be an admin to do that")
-const noSecretForThisParcel = createError("No secret for this parcel")
-const accessDenied = createError("Access denied")
+const noSuchUser = createError(101, "No such user")
+const noSuchTrip = createError(102, "No such trip")
+const noSuchParcel = createError(103, "No such parcel")
 
-function createError(msg) {
+const userExits = createError(201, "This user also exists")
+const notADriver = createError(202, "Not a driver")
+
+const wrongPassword = createError(301, "Wrong password")
+const requiredToken = createError(302, "Token is required")
+const invalidToken = createError(303, "Invalid token")
+const youNeedAdminRights = createError(304, "You need to be an admin to do that")
+const noSecretForThisParcel = createError(305, "No secret for this parcel")
+const accessDenied = createError(306, "Access denied")
+
+const driverHasActiveTrip = createError(401, "Driver has active an trip")
+const pointsAreTheSame = createError(402, "startPoint can't be equal to endPoint")
+
+function createError(code, msg) {
     return {
+        errorCode: code,
         errorMessage: msg
     }
 }
