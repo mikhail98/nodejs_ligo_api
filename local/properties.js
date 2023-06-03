189c2161d99ca9961371b52c5da9b7a47c2b867f
Mikhail Yarashevich <bigbottleapps@gmail.com>
2023-06-03 11:07:03 +0200

    v1.3

--- local/properties.js
+++ local/properties.js
@@ -1,8 +1,8 @@
-const MONGODB_URL = 'mongodb+srv://yrshvchstudio:nnAzaZwpALAOIyEB@pingocluster.jfl4hmk.mongodb.net/pingo?retryWrites=true&w=majority'
+const MONGODB_URL = ''
 
-const GOOGLE_PLACES_API_KEY = 'AIzaSyCSlmptrgECqtpqHpw1y7CzaqQWzMWUgto'
-const GOOGLE_DIRECTIONS_API_KEY = 'AIzaSyCx1eNQu54zl1ASzX5qu5CzjuPMF6FgC8w'
-const GOOGLE_SHEETS_API_KEY = 'AIzaSyCkWEudpATiJ-4V74wJ1Tjqjwo19DZdInU'
+const GOOGLE_PLACES_API_KEY = ''
+const GOOGLE_DIRECTIONS_API_KEY = ''
+const GOOGLE_SHEETS_API_KEY = ''
 
 module.exports = {
     MONGODB_URL,
