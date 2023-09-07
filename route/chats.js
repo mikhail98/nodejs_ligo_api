96ebc6cdb8dd80633d522d0a3301d7a0355aff92
Mikhail Yarashevich <bigbottleapps@gmail.com>
2023-09-07 13:15:00 +0200

    chat by id

--- route/chats.js
+++ route/chats.js
@@ -48,4 +48,14 @@ router.get('/:id/readMessages', log, auth, async (req, res) => {
     }
 })
 
+router.get('/:id', log, auth, async (req, res) => {
+    try {
+        const userId = req.user._id.toString()
+        return await ChatService.getChatById(userId, req.params.id, res)
+    } catch (error) {
+        console.log(error)
+        return res.status(400).send(Error.unknownError)
+    }
+})
+
 module.exports = router
